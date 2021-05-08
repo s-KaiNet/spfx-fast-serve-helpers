048f87030f9d184a9cb240a7237ebd0e8cafb6b6
Sergei Sergeev <sergeev.srg@gmail.com>
2021-05-08 17:40:37 +0300

    SPFx 1.12 and earlier support.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change Log
 
+## [1.12.0] - 08 May 2021
+
+### Features
+
+- SPFx 1.12.1 support and earlier
+
 ## [1.4.0] - 08 May 2021
 
 ### Features
--- src/gulp.ts
+++ src/gulp.ts
@@ -3,20 +3,11 @@ type Build = typeof import('@microsoft/sp-build-web');
 import { argv } from 'yargs';
 import { writeFileSync } from 'fs';
 import * as path from 'path';
-import * as del from 'del';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const workbenchApi = require('@microsoft/sp-webpart-workbench/lib/api');
 
 export function addFastServe(build: Build) {
   const useCustomServe = argv['custom-serve'];
-
-  const deleteDefinitions = build.subTask('delete-scss-definitions-task', function (gulp, buildOptions, done) {
-    del.sync(['src/**/*.scss.d.ts']);
-
-    done();
-  });
-
-  build.rig.addPreBuildTask(build.task('delete-scss-definitions', deleteDefinitions));
 
   if (!useCustomServe) return;
 
