84643be596e31e009f1f7f9c70f34fd5699faf95
Sergei Sergeev <sergeev.srg@gmail.com>
2021-05-09 01:02:23 +0300

    Added analytics tracking.

--- src/gulp.ts
+++ src/gulp.ts
@@ -3,6 +3,9 @@ type Build = typeof import('@microsoft/sp-build-web');
 import { argv } from 'yargs';
 import { writeFileSync } from 'fs';
 import * as path from 'path';
+import { hostname } from 'os';
+import { createHash } from 'crypto';
+import fetch from 'node-fetch';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const workbenchApi = require('@microsoft/sp-webpart-workbench/lib/api');
 
@@ -10,6 +13,8 @@ export function addFastServe(build: Build) {
   const useCustomServe = argv['custom-serve'];
 
   if (!useCustomServe) return;
+
+  trackAnalytics();
 
   build.tslintCmd.enabled = false;
 
@@ -50,4 +55,24 @@ export function addFastServe(build: Build) {
 
   build.rig.addPostTypescriptTask(saveConfigTask);
   build.rig.addPostBuildTask(ensureWorkbenchSubtask);
+}
+
+function trackAnalytics() {
+  try {
+    const hostNameHash = createHash('md5').update(hostname()).digest('hex');
+
+    fetch('https://fast-serve-track.azurewebsites.net/api/track', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-functions-key': 'TX0jA2PGYZQFJgeJeqSJbRcYwV7tx72vOD2LkICWxLe1O0f6KxxLaQ=='
+      },
+      body: JSON.stringify({
+        hostNameHash
+      })
+    });
+  }
+  catch (err) {
+    //
+  }
 }
