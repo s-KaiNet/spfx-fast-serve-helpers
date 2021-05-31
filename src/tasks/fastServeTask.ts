b6b57d5cceeccf0198fe183b8a1ea9c58a0fc86f
Sergei Sergeev <sergeev.srg@gmail.com>
2021-05-31 18:59:18 +0300

    Better handling for native serve command.

--- src/tasks/fastServeTask.ts
+++ src/tasks/fastServeTask.ts
@@ -1,10 +1,10 @@
-import { startDevServer } from '../webpack/devServer';
-
+import { fork } from 'child_process';
+import { resolve } from 'path';
 type Build = typeof import('@microsoft/sp-build-web');
 
 export function addFastServeTask(build: Build): void {
   const fastServeTask = build.subTask('fast-serve', function () {
-    startDevServer();
+    fork(resolve(__dirname, '../serve'));
   });
 
   build.rig.addPostBundleTask(fastServeTask);
