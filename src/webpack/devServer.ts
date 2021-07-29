d18199061069b9ece698abed5c84473cf9328380
Sergei Sergeev <sergeev.srg@gmail.com>
2021-07-29 22:53:20 +0300

    Added debug info.

--- src/webpack/devServer.ts
+++ src/webpack/devServer.ts
@@ -3,12 +3,15 @@
 import { resultConfig } from './configureWebPack';
 import Webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
+import { logDebugString } from './helpers';
 
 export async function startDevServer() {
   const config = await resultConfig();
   
   const compiler = Webpack(config);
   const server = new WebpackDevServer(compiler, config.devServer);
+
+  logDebugString();
 
   server.listen(config.devServer.port, config.devServer.host, (err) => {
     if (err) {
