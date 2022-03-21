428adc01181efd1e5f8d63ff33660211abcec5a4
Sergei Sergeev <sergeev.srg@gmail.com>
2022-03-21 12:41:27 +0300

    Basic implementation for a standalone fast-serve.

--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+
+import { spawn } from 'child_process';
 
 import { startDevServer } from './webpack/devServer';
 
-(async () => {
-  await startDevServer();
-})();
+const ls = spawn('gulp', ['bundle', '--custom-serve', '--max_old_space_size=4096'], {
+  env: process.env,
+  stdio: ['inherit'],
+  cwd: process.cwd(),
+  shell: true
+});
+
+ls.stdout.on('data', (data) => {
+  process.stdout.write(data.toString());
+});
+
+ls.stderr.on('data', (data) => {
+  process.stdout.write(data.toString());
+});
+
+ls.on('close', (code) => {
+  if (code !== 0) {
+    return;
+  }
+
+  (async () => {
+    await startDevServer();
+  })();
+});
