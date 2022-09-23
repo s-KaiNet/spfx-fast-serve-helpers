14b3a91a7c6378292b01de3f8e24ec2e64e3f96f
Sergei Sergeev <sergeev.srg@gmail.com>
2022-09-23 20:49:19 +0300

    Autoprefixing support.

--- src/tasks/saveConfigTask.ts
+++ src/tasks/saveConfigTask.ts
@@ -1,9 +1,8 @@
-type Build = typeof import('@microsoft/sp-build-web');
 import { writeFileSync, existsSync, mkdirSync } from 'fs';
 import * as path from 'path';
 
-export function addSaveConfigTask(build: Build): void {
-  const saveConfigTask = build.subTask('save-webpack-config', (gulp, config, done) => {
+export function addSaveConfigTask(build: any): void {
+  const saveConfigTask = build.subTask('save-webpack-config', (gulp: any, config: any, done: () => void) => {
     const serveAdditionalConfig = (generatedConfiguration: any) => {
       const saveDir = path.join(config.rootPath, 'temp');
       const saveTo = path.join(saveDir, '_webpack_config.json');
@@ -22,7 +21,7 @@ export function addSaveConfigTask(build: Build): void {
     } else {
       const oldConfigFunc = build.configureWebpack.taskConfig.additionalConfiguration;
       build.configureWebpack.mergeConfig({
-        additionalConfiguration: (generatedConfiguration) => {
+        additionalConfiguration: (generatedConfiguration: any) => {
           generatedConfiguration = oldConfigFunc(generatedConfiguration);
 
           return serveAdditionalConfig(generatedConfiguration);
