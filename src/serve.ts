d90446d310a462b06c2207754d95fec1cc382df7
Sergei Sergeev <sergeev.srg@gmail.com>
2023-09-17 15:56:01 +0200

    Fixed node 18 issue.

--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,30 @@
 #!/usr/bin/env node
 
-import { startDevServer } from './webpack/devServer';
+import { spawn } from 'cross-spawn';
+import * as path from 'path';
 
-(async () => {
-  await startDevServer();
-})();
+const env = { ...process.env };
+
+const nodeMajorVersion = parseInt(process.version.split('.')[0].substring(1), 10);
+
+if (nodeMajorVersion >= 17) {
+  env['NODE_OPTIONS'] = '--openssl-legacy-provider';
+}
+
+const proc = spawn('node', [path.resolve(__dirname, 'webpack/devServer.js')], {
+  stdio: 'inherit',
+  env
+})
+
+process.on('SIGTERM', () => proc.kill('SIGTERM'))
+process.on('SIGINT', () => proc.kill('SIGINT'))
+process.on('SIGBREAK', () => proc.kill('SIGBREAK'))
+process.on('SIGHUP', () => proc.kill('SIGHUP'))
+
+proc.on('exit', (code, signal) => {
+  let crossEnvExitCode = code
+  if (crossEnvExitCode === null) {
+    crossEnvExitCode = signal === 'SIGINT' ? 0 : 1
+  }
+  process.exit(crossEnvExitCode)
+});
